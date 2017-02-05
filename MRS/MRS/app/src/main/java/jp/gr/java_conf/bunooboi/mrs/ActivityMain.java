638c66f1602368760e93db681631bd7395efe8db
bunooboi <bunooboi@gmail.com>
2017-02-05 19:35:08 +0900

    mrs

--- MRS/MRS/app/src/main/java/jp/gr/java_conf/bunooboi/mrs/ActivityMain.java
+++ MRS/MRS/app/src/main/java/jp/gr/java_conf/bunooboi/mrs/ActivityMain.java
@@ -1,8 +1,12 @@
 package jp.gr.java_conf.bunooboi.mrs;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -22,6 +26,7 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("復習データ");
 
+        verifyStoragePermissions(this);
         Memorys.init();
 
         listView = (ListView) findViewById(R.id.listView);
@@ -81,5 +86,25 @@ public class ActivityMain extends AppCompatActivity {
                 break;
         }
         return false;
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
