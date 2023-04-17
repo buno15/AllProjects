4e4fca6ba25c4d1edf093fe1017f34f3d040207a
buno15 <bunooboi@gmail.com>
2023-04-18 00:36:08 +0900

    add

--- practice/ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
+++ practice/ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +36,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AppScreen() {
-
+    Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = painterResource(id = R.drawable.icon), contentDescription = "icon", modifier = Modifier
+            .border(2.dp, Color.Black)
+            .weight(1f), contentScale = ContentScale.Crop)
+        Image(painter = painterResource(id = R.drawable.icon), contentDescription = "icon", modifier = Modifier
+            .border(2.dp, Color.Black)
+            .weight(2f)
+            .align(Alignment.Top), contentScale = ContentScale.Crop)
+        Image(painter = painterResource(id = R.drawable.icon), contentDescription = "icon", modifier = Modifier
+            .border(2.dp, Color.Black)
+            .weight(1f), contentScale = ContentScale.Crop)
+    }
 }
 
 
