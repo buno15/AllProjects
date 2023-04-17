a56b7eb45555d66938ac55bc6ae09be50427b777
buno15 <bunooboi@gmail.com>
2023-04-17 23:00:25 +0900

    add practice

--- app/AAOJ/app/src/androidTest/java/com/example/aaoj/ExampleInstrumentedTest.kt
+++ app/AAOJ/app/src/androidTest/java/com/example/aaoj/ExampleInstrumentedTest.kt
@@ -1,12 +1,24 @@
 package com.example.aaoj
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.util.Log
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
+import org.hamcrest.Matchers.*
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +27,42 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    private lateinit var device: UiDevice
+
+    @Before
+    fun setUp() {
+        // UiDevice インスタンスを取得。
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.aaoj", appContext.packageName)
+    fun mainActivityTest() {
+        val edittext = onView(withId(R.id.edittext_pid))
+        edittext.perform(typeText("0123"))
+
+        val cond = Until.hasObject(By.res("com.example.AAOJ", "button_search"))
+
+        device.wait(cond, 1000L)
+
+        val button = onView(withId(R.id.button_search))
+        button.perform(click())
+
+        device.wait(cond, 2000L)
+
+        edittext.check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun writeSurface() {
+        onView(withId(R.id.fab_change)).perform(click())
+
+        onView(withId(R.id.surfaceView)).perform(swipeDown())
+        onView(withId(R.id.surfaceView)).perform(swipeLeft())
+
+        val cond = Until.hasObject(By.res("com.example.AAOJ", "surfaceView"))
+        device.wait(cond, 2000L)
     }
 }
