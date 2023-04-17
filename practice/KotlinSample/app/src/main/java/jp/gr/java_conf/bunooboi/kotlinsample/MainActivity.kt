548c45544342d8d0154e84c8c83d9d8c8b6bcc11
buno15 <bunooboi@gmail.com>
2023-04-17 23:01:50 +0900

    add

---
+++ practice/KotlinSample/app/src/main/java/jp/gr/java_conf/bunooboi/kotlinsample/MainActivity.kt
@@ -0,0 +1,16 @@
+package jp.gr.java_conf.bunooboi.kotlinsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setText("button")
+    }
+}
