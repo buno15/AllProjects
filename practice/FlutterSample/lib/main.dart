96ab74ad317a0531cdf81dd736544e0a3d27ea05
buno15 <bunooboi@gmail.com>
2023-09-02 00:05:12 +0900

    change todo to firebase

--- practice/FlutterSample/lib/main.dart
+++ practice/FlutterSample/lib/main.dart
@@ -1,72 +1,135 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'add_page.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyTodoApp());
+Future<void> main() async {
+  // Firebase初期化
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
-class MyTodoApp extends StatelessWidget {
-  const MyTodoApp({super.key});
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Todo App',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TodoListPage(),
+      home: MyAuthPage(),
     );
   }
 }
 
-class TodoListPage extends StatefulWidget {
-  const TodoListPage({super.key});
-
+class MyAuthPage extends StatefulWidget {
   @override
-  State<TodoListPage> createState() => _TodoListPageState();
+  _MyAuthPageState createState() => _MyAuthPageState();
 }
 
-class _TodoListPageState extends State<TodoListPage> {
-  List<String> todoList = [];
+class _MyAuthPageState extends State<MyAuthPage> {
+  String newUserEmail = "";
+  String newUserPassword = "";
+  String loginUserEmail = "";
+  String loginUserPassword = "";
+  String infoText = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("リスト一覧"),
-        actions: null,
-      ),
       body: Center(
-        child: ListView.builder(
-          itemCount: todoList.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                title: Text(todoList[index]),
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                    return TodoAddPage();
-                  }));
+        child: Container(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
                 },
               ),
-            );
-          },
+              const SizedBox(
+                height: 8,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: "パスワード"),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final UserCredential result = await auth.createUserWithEmailAndPassword(email: newUserEmail, password: newUserPassword);
+
+                      final User user = result.user!;
+                      setState(() {
+                        infoText = "登録OK：${user.email}";
+                      });
+                    } catch (e) {
+                      setState(() {
+                        infoText = "登録NG：${e.toString()}";
+                      });
+                    }
+                  },
+                  child: const Text("ユーザー登録")),
+              const SizedBox(
+                height: 32,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    loginUserEmail = value;
+                  });
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード"),
+                onChanged: (String value) {
+                  setState(() {
+                    loginUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result = await auth.signInWithEmailAndPassword(email: loginUserEmail, password: loginUserPassword);
+
+                    final User user = result.user!;
+                    setState(() {
+                      infoText = "ログインOK：${user.email}";
+                    });
+                  } catch (e) {
+                    setState(() {
+                      infoText = "ログインNG：${e.toString()}";
+                    });
+                  }
+                },
+                child: Text("ログイン"),
+              ),
+              Text(infoText),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final newListText = await Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            return TodoAddPage();
-          }));
-          if (newListText != null) {
-            setState(() {
-              todoList.add(newListText);
-            });
-          }
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
