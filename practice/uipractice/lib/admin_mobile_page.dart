899b8f04dad68e282ee32bd1e5e3bea8a238c2a4
buno15 <bunooboi@gmail.com>
2023-09-04 13:16:42 +0900

    add AdminPage

--- practice/uipractice/lib/admin_mobile_page.dart
+++ practice/uipractice/lib/admin_mobile_page.dart
@@ -7,7 +7,267 @@ class AdminMobilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Row(
+        children: [
+          SideNavigation(),
+          VerticalDivider(thickness: 1, width: 1),
+          Expanded(child: PostList()),
+        ],
+      ),
+    );
+  }
+}
+
+class SideNavigation extends StatefulWidget {
+  @override
+  _SideNavigationState createState() => _SideNavigationState();
+}
+
+class _SideNavigationState extends State<SideNavigation> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      destinations: const [
+        NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text("ThumbsUpDown"),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.people),
+          label: Text("People"),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.face),
+          label: Text("Face"),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.bookmark),
+          label: Text("Bookmark"),
+        )
+      ],
+    );
+  }
+}
+
+class _PostsHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            leading: ClipOval(
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(Icons.storage, color: Colors.grey[800]),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            leading: ClipOval(
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(
+                  Icons.style,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            title: Text("All Types"),
+            subtitle: Text(""),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Post extends StatelessWidget {
+  final String name;
+  final String message;
+  final String textReason;
+  final Color colorPrimary;
+  final Color colorPositive;
+  final String textPositive;
+  final Color colorNegative;
+  final String textNegative;
+
+  const _Post({
+    required Key key,
+    required this.name,
+    required this.message,
+    required this.textReason,
+    required this.colorPrimary,
+    required this.colorPositive,
+    required this.textPositive,
+    required this.colorNegative,
+    required this.textNegative,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      child: Card(
+        elevation: 8,
+        shadowColor: Colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              leading: ClipOval(
+                child: Container(
+                  color: colorPrimary,
+                  width: 48,
+                  height: 48,
+                  child: Center(
+                    child: Text(
+                      name.substring(0, 1),
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+              title: Text(name),
+              subtitle: Text("2 min ago"),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 72),
+                  Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: colorPrimary, width: 4),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Flexible(
+                    child: Text(message),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: colorPrimary, width: 2),
+                      ),
+                    ),
+                    child: Text(
+                      textReason,
+                      style: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(primary: colorNegative),
+                      onPressed: () {},
+                      child: Text(textNegative),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(primary: colorPositive, backgroundColor: colorPositive.withOpacity(0.2)),
+                      onPressed: () {},
+                      child: Text(textPositive),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PostGreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _Post(
+      key: Key("green"),
+      name: "Pean",
+      message: "I'm a green post",
+      textReason: "Report Details",
+      colorPrimary: Colors.greenAccent,
+      colorPositive: Colors.greenAccent,
+      textPositive: "Keep",
+      colorNegative: Colors.blueAccent,
+      textNegative: "Archive",
+    );
+  }
+}
+
+class _PostRed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _Post(
+      key: Key("red"),
+      name: 'Namaga Tema',
+      message: 'Not recomended for publication.',
+      textReason: 'Pending Reason',
+      colorPrimary: Colors.deepOrangeAccent,
+      colorPositive: Colors.blueAccent,
+      textPositive: 'Publish',
+      colorNegative: Colors.deepOrangeAccent,
+      textNegative: 'Decline',
+    );
+  }
+}
+
+class PostList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 48),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _PostsHeader(),
+          Expanded(
+            child: ListView(
+              children: [
+                _PostGreen(),
+                _PostRed(),
+                _PostGreen(),
+                _PostRed(),
+                _PostGreen(),
+                _PostRed(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
